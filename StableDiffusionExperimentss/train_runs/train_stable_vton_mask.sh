b429b23236872bc117f9ff9c16abdf628c9b8304
ankitbelbase17 <ankitbelbase034@gmail.com>
2026-05-06 18:26:45 +0545

    new_env_created

--- StableDiffusionExperimentss/train_runs/train_stable_vton_mask.sh
+++ StableDiffusionExperimentss/train_runs/train_stable_vton_mask.sh
@@ -23,7 +23,7 @@ unset PYTHONHOME || true
 unset PYTHONPATH || true
 
 CONDA_ROOT="/iopsstor/scratch/cscs/dbartaula/miniforge3"
-CONDA_ENV_NAME="${CONDA_ENV_NAME:-torch28_env_new}"
+CONDA_ENV_NAME="${CONDA_ENV_NAME:-stablevton_orig}"
 if [ -f "$CONDA_ROOT/etc/profile.d/conda.sh" ]; then
   source "$CONDA_ROOT/etc/profile.d/conda.sh"
 else
